0368c05ce67c39e5f968c3d40d467046d901a85f
Mitch Schwenk <mitch@yombo.net>
2017-02-04 18:58:30 -0800

    Add additional help menu items. Make display a little cleaner.

---
+++ yombo/lib/webinterface/fragments/datacard_no_label_css.tpl
@@ -0,0 +1,17 @@
+<style>
+.card-view {
+    clear: both;
+}
+
+.card-view .title {
+    display: block;
+    width: 0%;
+    float: left;
+}
+
+.card-view .value {
+    display: block;
+    width: 100%;
+    float: right;
+}
+</style>
--- yombo/lib/webinterface/route_devtools.py
+++ yombo/lib/webinterface/route_devtools.py
@@ -23,40 +23,6 @@ def route_devtools(webapp):
             return page.render(alerts=webinterface.get_alerts(),
                                )
 
-        @webapp.route('/commands/public')
-        @require_auth()
-        def page_devtools_commands_public(webinterface, request, session):
-            page = webinterface.get_template(request, webinterface._dir + 'pages/devtools/commands_list.html')
-            return page.render(alerts=webinterface.get_alerts(),
-                               commands=webinterface._Commands.get_public_commands(),
-                               page_label='Public Commands',
-                               page_description='Publicly available commands.'
-                               )
-
-        @webapp.route('/commands/local')
-        @require_auth()
-        def page_devtools_commands_local(webinterface, request, session):
-            page = webinterface.get_template(request, webinterface._dir + 'pages/devtools/commands_list.html')
-            return page.render(alerts=webinterface.get_alerts(),
-                               commands=webinterface._Commands.get_local_commands(),
-                               page_label='Local Commands',
-                               page_description='Local commands, only available to the primary account holder.'
-                               )
-
-        @webapp.route('/commands/edit/<string:command_id>')
-        @require_auth()
-        def page_devtools_command_details(webinterface, request, session, command_id):
-            try:
-                command = webinterface._Commands[command_id]
-            except:
-                webinterface.add_alert('Command ID was not found.', 'warning')
-                return webinterface.redirect(request, '/devtools/command/index')
-            page = webinterface.get_template(request, webinterface._dir + 'pages/devtools/command_edit.html')
-            return page.render(alerts=webinterface.get_alerts(),
-                               command=command,
-                               input_types=webinterface._InputTypes.get_all()
-                               )
-
         @webapp.route('/debug')
         @require_auth()
         def page_devtools_debug(webinterface, request, session):
@@ -374,6 +340,7 @@ def route_devtools(webapp):
                 'description': webinterface.reqest_get_default(request, 'description', ""),
                 'status': int(webinterface.reqest_get_default(request, 'status', 1)),
                 'public': int(webinterface.reqest_get_default(request, 'public', 0)),
+                'id': command_id,
             }
 
             command_results = yield webinterface._Commands.dev_edit_command(command_id, data)
@@ -382,6 +349,12 @@ def route_devtools(webapp):
 
             if command_results['status'] == 'failed':
                 webinterface.add_alert(command_results['apimsghtml'], 'warning')
+                root_breadcrumb(webinterface, request)
+                webinterface.add_breadcrumb(request, "/devtools/commands/index", "Commands")
+                webinterface.add_breadcrumb(request, "/devtools/commands/%s/details" % command_id,
+                                            data['label'])
+                webinterface.add_breadcrumb(request, "/devtools/commands/%s/edit", "Edit")
+
                 returnValue(page_devtools_commands_form(webinterface, request, session, 'edit', data,
                                                             "Edit Command: %s" % data['label']))
 
