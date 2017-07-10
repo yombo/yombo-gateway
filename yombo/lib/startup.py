4f11c453c826f0943ed742316c4d96c8818fc84d
Mitch Schwenk <mitch@yombo.net>
2017-07-10 02:47:14 -0700

    Optimize the layout.

--- yombo/lib/startup.py
+++ yombo/lib/startup.py
@@ -29,32 +29,41 @@ class Startup(YomboLibrary):
     """
 #    @inlineCallbacks
     def _init_(self, **kwargs):
-        if self._Loader.operation_mode != None:  # will know if firstrun already or yombo.ini is missing.
+        print("Startup: self._Loader.operating_mode: %s" % self._Loader.operating_mode)
+        first_run = self._Configs.get('core', 'first_run', False, False)
+        print("Startup: first_run: %s" % first_run)
+        if self._Loader.operating_mode == 'first_run':  # will know if first_run already or yombo.ini is missing.
             return
-        need_config = False
+        if first_run is True:
+            self._Loader.operating_mode = 'first_run'
+            return True
+        items_needed = []
+
+        gwid = self._Configs.get("core", "gwid", None)
+        if gwid is None or gwid == "":
+            items_needed.append("Gateway ID")
         gwuuid = self._Configs.get("core", "gwuuid", None)
         if gwuuid is None or gwuuid == "":
-            self._Libraries['webinterface'].add_alert("No gateway ID, entering 'config' mode.", 'warning', type='system')
-            logger.error("No gateway ID, entering 'config' mode.")
-            need_config = True
-
+            items_needed.append("Gateway private UUID")
         hash = self._Configs.get("core", "gwhash", None)
         if hash is None or hash == "":
-            self._Libraries['webinterface'].add_alert("No gateway hash, entering 'config' mode.", 'warning', type='system')
-            logger.error("No gateway hash, entering 'config' mode.")
-            need_config = True
-
+            items_needed.append("Gateway login password hash")
         gpg_key = self._Configs.get("gpg", "keyid", None)
         gpg_key_ascii = self._Configs.get("gpg", "keyascii", None)
         if gpg_key is None or gpg_key == '' or gpg_key_ascii is None or gpg_key_ascii == '':
-            self._Libraries['webinterface'].add_alert("No GPG/PGP key pair found, entering 'config' mode.", 'warning', type='system')
-            logger.error("No GPG/PGP key pair found, entering 'config' mode.")
-            need_config = True
+            items_needed.append("GPG keys")
 
-        first_run = self._Configs.get('core', 'firstrun', True)
-        if first_run:
-            self._Loader.operation_mode = 'firstrun'
-        elif need_config:
-            self._Loader.operation_mode = 'config'
+        if len(items_needed) > 0:
+            needed_text = '</li><li>'.join(items_needed)
+            print("start needetext: %s" % needed_text)
+            self._Notifications.add({'title': 'Need configurations',
+                                     'message': 'System has been places into configuration mode. Reason: The following configurations are needed:<p><ul><li>%s</li></ul>' % needed_text,
+                                     'source': 'Yombo Startup Library',
+                                     'persist': False,
+                                     'priority': 'high',
+                                     'always_show': True,
+                                     'always_show_allow_clear': True,
+                                     })
+            self._Loader.operating_mode = 'config'
         else:
-            self._Loader.operation_mode = 'run'
+            self._Loader.operating_mode = 'run'
