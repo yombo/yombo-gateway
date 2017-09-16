fc4db088fcab551e83199b637937f74a91b3501b
Mitch Schwenk <mitch@yombo.net>
2017-09-16 11:35:11 -0700

    Get debian installer up to speed.

--- install/debian-setup.sh
+++ install/debian-setup.sh
@@ -32,6 +32,7 @@ read -e -p "Git Repo: " -i "https://bitbucket.org/yombo/yombo-gateway.git" repol
 echo ""
 echo "Using git repo location: $repolocation";
 
+sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
 sudo apt-get update
 sudo apt-get upgrade -y
 
@@ -39,10 +40,36 @@ sudo apt-get install make libudev-dev g++ libssl-dev zlib1g-dev libbz2-dev libre
 libreadline6-dev libreadline-dev libsqlite3-dev libexpat1-dev liblzma-dev \
 python python-dev python3-dev gnupg2 rng-tools build-essential git \
 python3-setuptools python3-pip python-pip libyaml-dev libncurses5 \
-libncurses5-dev libncursesw5 libncursesw5-dev xz-utils curl wget llvm tk-dev libbluetooth-dev -y
+libncurses5-dev libncursesw5 libncursesw5-dev xz-utils curl wget llvm \
+tk-dev libbluetooth-dev mosquitto libmosquitto-dev libcurl4-openssl-dev \
+libc-ares-dev uuid-dev daemon quilt -y
+
+# we install mosquitto to get the services installed and configs installed.
+# but later we will manually install a version for updated features and
+# to use an updated libwebsockets
+sudo apt-mark hold mosquitto
 
 sudo pip3 install --upgrade pip
 sudo usermod -a -G dialout $SUDO_USER
+
+cd /usr/local/src
+sudo mkdir yombo
+sudo chown $SUDO_USER yombo
+cd yombo
+git clone https://github.com/warmcat/libwebsockets.git
+cd libwebsockets
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
+
+cd /usr/local/src/yombo
+git clone https://github.com/eclipse/mosquitto.git
+cd mosquitto
+make binary WITH_WEBSOCKETS=yes WITH_DOCS=no CFLAGS=-I/usr/local/include/
+sudo make install WITH_WEBSOCKETS=yes WITH_DOCS=no CFLAGS=-I/usr/local/include/
 
 cwd=$(pwd)
 
