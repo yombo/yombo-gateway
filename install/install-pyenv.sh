8473098ba2e9dc2151cf89c9f64374c867b4dfe6
Mitch Schwenk <mitch@yombo.net>
2017-11-10 18:37:30 -0800

    Bump installer to python 3.6.3

--- install/install-pyenv.sh
+++ install/install-pyenv.sh
@@ -9,7 +9,7 @@ if [ "$(id -u)" -e 0 ]; then
     exit
 fi
 
-echo "This will download and install pyenv as well as python 3.6.2."
+echo "This will download and install pyenv as well as python 3.6.3."
 echo ""
 echo "This will take a while due to compiling the needed requirements."
 while true; do
@@ -21,6 +21,18 @@ while true; do
     esac
 done
 
+sudo apt-get update
+sudo apt-get install --force-yes -y make libudev-dev g++ libyaml-dev
+sudo apt-get install python3-pip python3-setuptools python3-dev gnupg2 rng-tools build-essential git libncurses5 libncurses5-dev libncursesw5 \
+libncursesw5-dev xz-utils libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl wget llvm tk-dev -y
+
+export PATH="/home/$USER/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+env | grep PATH
+
+CFLAGS='-O2'
+
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
@@ -31,8 +43,7 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-pyenv install 3.6.2
-cd /opt/yombo-gateway
-pyenv local 3.6.2
-
-pip3 isntall --upgrade -r requirements.txt
+pyenv install 3.6.3
+pyenv local 3.6.3
+mv .python-version ../
+pip install -r ../requirements.txt
