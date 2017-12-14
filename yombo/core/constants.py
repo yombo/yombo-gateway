43de01120a355e724eb3b8952a29226fb0e464b2
Mitch Schwenk <mitch@yombo.net>
2017-12-13 23:00:54 -0800

    Add version.

--- yombo/core/constants.py
+++ yombo/core/constants.py
@@ -4,11 +4,15 @@
 Various constants used throughout the system.
 
 .. moduleauthor:: Mitch Schwenk <mitch-gw@yombo.net>
+.. versionadded:: 0.14.0
+
 
 :copyright: Copyright 2017 by Yombo.
 :license: LICENSE for details.
 :view-source: `View Source Code <https://yombo.net/docs/gateway/html/current/_modules/yombo/core/constants.html>`_
 """
+VERSION = "0.15.0"
+
 WEEKDAYS = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 
 MISC_UNIT_SYSTEM_METRIC = 'metric'  # type: str
