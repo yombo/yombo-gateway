56583f6b630f7009d9a60618601e47fc0bead992
Mitch Schwenk <mitch@yombo.net>
2017-12-15 01:16:42 -0800

    Removed pipe char from extended chars set.

--- yombo/utils/__init__.py
+++ yombo/utils/__init__.py
@@ -1116,7 +1116,7 @@ def random_string(**kwargs):
     if 'char_set' in kwargs:
         char_set = kwargs['char_set']
         if char_set == 'extended':
-            letters = "abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%()*-;<=>@^-{}|~"
+            letters = "abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%()*-;<=>^-{}~"
     else:
         letters = kwargs.get('letters', None)
 
