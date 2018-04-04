e88a6f227e9a6058ab3974cfe70a0dcbd2c41194
Mitch Schwenk <mitch@yombo.net>
2018-04-03 23:56:26 -0700

    Add more constants.

--- yombo/constants/features.py
+++ yombo/constants/features.py
@@ -1,19 +1,24 @@
 # Device features
 FEATURE_ALL_ON = 'all_on'
 FEATURE_ALL_OFF = 'all_off'
+FEATURE_ALLOW_IN_SCENES = 'allow_in_scenes'
 FEATURE_BRIGHTNESS = 'brightness'
 FEATURE_COLOR_TEMP = 'color_temp'
+FEATURE_DETECTS_MOTION = 'detects_motion'
 FEATURE_EFFECT = 'effect'
+FEATURE_FAN_MODE = "fan_mode"
+FEATURE_MODES = "modes"
 FEATURE_NUMBER_OF_STEPS = 'number_of_steps'
+FEATURE_OPERATION_MODE = "operation_mode"
 FEATURE_PERCENT = 'percent'
-FEATURE_RGB_COLOR = 'rgb_color'
-FEATURE_TRANSITION = 'transition'
-FEATURE_WHITE_VALUE = 'white_value'
-FEATURE_XY_COLOR = 'xy_color'
 FEATURE_POWER_CONTROL = 'power_control'
 FEATURE_PINGABLE = 'pingable'
 FEATURE_POLLABLE = 'pollable'
 FEATURE_SEND_UPDATES = 'sends_updates'
-
-
-
+FEATURE_RGB_COLOR = 'rgb_color'
+FEATURE_THERMOSTAT = 'thermostat'
+FEATURE_TRANSITION = 'transition'
+FEATURE_WHITE_VALUE = 'white_value'
+FEATURE_XY_COLOR = 'xy_color'
+FEATURE_CHANNEL_CONTROL = 'channel_control'
+FEATURE_INPUT_CONTROL = 'input_control'
