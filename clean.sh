468ce3b84ae224a373b234dbd85b561c8b69d509
Mitch Schwenk <mitch@yombo.net>
2018-06-18 10:30:22 -0700

    Move clear.sh to scripts/clear_cache. Integrate with ybo command.

--- clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Used by developers to quickly clean up the folder structure.
-echo "This script simply cleans up cached compiled python code files."
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-echo "[-] Local python cache files have been removed."
