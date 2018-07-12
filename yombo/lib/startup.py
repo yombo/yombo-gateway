b35424ab429e9c2ff7f27e53c7ac0dc79633d540
Mitch Schwenk <mitch@yombo.net>
2018-07-12 00:45:27 -0700

    Update startup to actually handle more startup items.
    This now asks YomboAPI library to validate the gateway auth items.
    If it's bad, it attemps vairous methods to re-auth the gateway to
    the yombo cloud.

--- yombo/lib/startup.py
+++ yombo/lib/startup.py
@@ -21,14 +21,14 @@ mode.
 :license: LICENSE for details.
 :view-source: `View Source Code <https://yombo.net/Docs/gateway/html/current/_modules/yombo/lib/startup.html>`_
 """
-# Import Yombo libraries
-from yombo.core.exceptions import YomboWarning
-from yombo.core.library import YomboLibrary
-from yombo.core.log import get_logger
-
 # Import twisted libraries
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import LoopingCall
+
+# Import Yombo libraries
+from yombo.core.exceptions import YomboWarning, YomboRestart
+from yombo.core.library import YomboLibrary
+from yombo.core.log import get_logger
 
 from yombo.utils import sleep
 
@@ -48,39 +48,35 @@ class Startup(YomboLibrary):
         """
         return "Yombo startup library"
 
+    @inlineCallbacks
     def _init_(self, **kwargs):
+        self.gwid = self._Configs.get2('core', 'gwid', 'local', False)
+        self.gwuuid = self._Configs.get2("core", "gwuuid", None, False)
+        self.gwhash = self._Configs.get2("core", "gwhash", None, False)
+        self.api_auth = self._Configs.get2("core", "api_auth", None, False)
+        self.has_valid_gw_auth = False
+
         self.cache_updater_running = False
         self.system_stopping = False
-        first_run = self._Configs.get('core', 'first_run', False, False)
         if self._Loader.operating_mode == 'first_run':  # will know if first_run already or yombo.ini is missing.
             return
+        first_run = self._Configs.get('core', 'first_run', False, False)
         if first_run is True:
             self._Loader.operating_mode = 'first_run'
             return True
-        items_needed = []
 
-        gwid = self._Configs.get('core', 'gwid', 'local', False)
-        if gwid is None or gwid == "":
-            items_needed.append("Gateway ID")
-        gwuuid = self._Configs.get("core", "gwuuid", None)
-        if gwuuid is None or gwuuid == "":
-            items_needed.append("Gateway private UUID")
-        hash = self._Configs.get("core", "gwhash", None)
-        if hash is None or hash == "":
-            items_needed.append("Gateway login password hash")
+        operating_mode, items_needed = yield self.check_has_valid_gw_auth()
+
         is_master = self._Configs.get("core", "is_master", True)
         if is_master is False:
             master_gateway = self._Configs.get("core", "master_gateway", None, False)
             if master_gateway is None or master_gateway == "":
-                items_needed.append("Gateway is marked as slave, but not master gateway set.")
-        else:
-            master_gateway = self._Configs.get("core", "master_gateway", None, False)
+                items_needed.append("Gateway is marked as slave, but no master gateway set.")
 
         if len(items_needed) > 0:
             needed_text = '</li><li>'.join(items_needed)
-            print("start needetext: %s" % needed_text)
             self._Notifications.add({'title': 'Need configurations',
-                                     'message': 'System has been placed into configuration mode. Reason: The following configurations are needed:<p><ul><li>%s</li></ul>' % needed_text,
+                                     'message': 'System has been placed into configuration mode. The following configurations are needed:<p><ul><li>%s</li></ul>' % needed_text,
                                      'source': 'Yombo Startup Library',
                                      'persist': False,
                                      'priority': 'high',
@@ -89,7 +85,57 @@ class Startup(YomboLibrary):
                                      })
             self._Loader.operating_mode = 'config'
         else:
-            self._Loader.operating_mode = 'run'
+            self._Loader.operating_mode = operating_mode
+        yield self._GPG._init_from_startup_()
+
+    @inlineCallbacks
+    def check_has_valid_gw_auth(self, session=None):
+        @inlineCallbacks
+        def try_get_api_auth_keys(try_session):
+            try:
+                yield self._YomboAPI.get_api_auth_keys(session=try_session)
+            except YomboRestart:
+                logger.error("System going down for restart, have new auth credentials")
+                yield sleep(120)
+                return operating_mode, items_needed
+            except YomboWarning:
+                pass
+
+        operating_mode = 'run'
+        items_needed = []
+        gwid = self.gwid()
+        if gwid is None or gwid == "":
+            items_needed.append("Gateway ID is missing. Please complete the setup wizard again.")
+            operating_mode = 'first_run'
+            return operating_mode, items_needed
+        gwuuid = self.gwuuid()
+        if gwuuid is None or gwuuid == "":
+            operating_mode = 'config'
+            items_needed.append("Gateway UUID is missing.")
+        gwhash = self.gwhash()
+        if gwhash is None or gwhash == "":
+            operating_mode = 'config'
+            items_needed.append("Gateway password is missing.")
+
+        is_valid_system = yield self._YomboAPI.check_api_auth_valid()
+        if is_valid_system is False:
+            # System doesn't have valid auth info. Lets search through the session database and try to
+            # find a user that will has access to refresh our auth information.
+
+            sessions = yield self._LocalDB.get_web_session()
+            if session is not None and isinstance(session, str):
+                yield try_get_api_auth_keys(session)
+            for session in sessions:
+                data = session['session_data']
+                if 'yomboapi_session' in data and isinstance(data['yomboapi_session'], str):
+                    yield try_get_api_auth_keys(data['yomboapi_session'])
+
+            # If we here, then no valid session was found. Put system into config mode. It will be attempted
+            # when the user access its. We will also display a notice.
+            operating_mode = 'config'
+            items_needed.append('System is unable to authenticate itself with the server. The owner simply needs to'
+                                ' log into the system. This will activate the reauthorization.')
+        return operating_mode, items_needed
 
     def _start_(self, **kwargs):
         """
