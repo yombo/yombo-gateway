d5924f6ccd1e01e4a36a1584dbb3ca8c558ebc73
Mitch Schwenk <mitch@yombo.net>
2018-11-06 12:29:20 -0800

    Add requests constants.

---
+++ yombo/constants/requests.py
@@ -0,0 +1,8 @@
+# Various request headers
+HEADER_CONTENT_TYPE = "Content-Type"
+HEADER_AUTHORIZATION = "Authorization"
+HEADER_X_API_KEY = "x-api-key"
+HEADER_USER_AGENT = "User-Agent"
+
+HTTP_BASIC_AUTHENTICATION = 'basic'
+HTTP_DIGEST_AUTHENTICATION = 'digest'
