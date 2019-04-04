8f456c43977da8f5d16e893711f23e9874266729
Mitch Schwenk <mitch@yombo.net>
2019-04-04 09:03:49 -0700

    Add: Add support for storing access and refresh tokens inside websessions. This saves to the database.

--- yombo/lib/localdb/websessions.py
+++ yombo/lib/localdb/websessions.py
@@ -23,11 +23,13 @@ class DB_Websessions(object):
             return {
                 "id": data.id,
                 "enabled": coerce_value(data.enabled, "bool"),
-                "user_id": data.user_id,
-                "auth_at": save_data.get("auth_at", 0),
-                "created_by": save_data.get("created_by", "unknown"),
                 "gateway_id": data.gateway_id,
-                "auth_data": save_data.get("auth_data", {}),
+                "user_id": data.user_id,
+                "auth_data": save_data.save_data,
+                "refresh_token": data._refresh_token,
+                "access_token": data._access_token,
+                "refresh_token_expires_at": data.refresh_token_expires_at,
+                "access_token_expires_at": data.access_token_expires_at,
                 "created_at": data.created_at,
                 "last_access_at": data.last_access_at,
                 "updated_at": data.updated_at,
@@ -50,7 +52,6 @@ class DB_Websessions(object):
         save_data = data_pickle({
             "auth_data": session.auth_data,
             "auth_at": session.auth_at,
-            "created_by": session.created_by,
         })
 
         args = {
@@ -58,6 +59,10 @@ class DB_Websessions(object):
             "enabled": coerce_value(session.enabled, "int"),
             "gateway_id": session.gateway_id,
             "auth_data": save_data,
+            "refresh_token": session._refresh_token,
+            "access_token": session._access_token,
+            "refresh_token_expires_at": session.refresh_token_expires_at,
+            "access_token_expires_at": session.access_token_expires_at,
             "user_id": session.user_id,
             "created_at": session.created_at,
             "last_access_at": session.last_access_at,
@@ -72,11 +77,14 @@ class DB_Websessions(object):
             "auth_data": session.auth_data,
             "auth_type": session.auth_type,
             "auth_at": session.auth_at,
-            "created_by": session.created_by,
         })
         args = {
             "enabled": coerce_value(session.enabled, "int"),
             "auth_data": save_data,
+            "refresh_token": session._refresh_token,
+            "access_token": session._access_token,
+            "refresh_token_expires_at": session.refresh_token_expires_at,
+            "access_token_expires_at": session.access_token_expires_at,
             "user_id": session.user_id,
             "last_access_at": session.last_access_at,
             "updated_at": session.updated_at,
