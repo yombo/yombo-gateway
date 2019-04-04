6572cd976659c13015ae1c9c56f2d8ea782c5a25
Mitch Schwenk <mitch@yombo.net>
2019-04-04 15:36:01 -0700

    Syntax: Removing junt print line statements from systemdatahandler. Add some debuging lines.

--- yombo/lib/systemdatahandler/__init__.py
+++ yombo/lib/systemdatahandler/__init__.py
@@ -80,13 +80,13 @@ class SystemDataHandler(YomboLibrary, object):
         # print("about to download system data....")
         self.bulk_load = True
         self.db_existing_ids = yield self._LocalDB.get_ids_for_remote_tables()
-        # print(f"Existing IDs: {self.db_existing_ids}")
+        logger.debug("Existing IDs: {existing_ids}", existing_ids=self.db_existing_ids)
 
         if routes is None:
             routes = list(self.api_routes.keys())
 
         for route in routes:
-            # print(f"getting data from: {self.api_routes[route]}")
+            logger.debug("getting data from: {route}", route=self.api_routes[route])
             d = self.download_semaphore.run(self._YomboAPI.request, "GET", self.api_routes[route])
             d.addCallback(self.download_semaphore_process_results)
             self.download_deferreds.append(d)
@@ -95,7 +95,6 @@ class SystemDataHandler(YomboLibrary, object):
         yield dl
 
     def download_semaphore_process_results(self, data):
-        # print("download_semaphore_process_results")
         if "data" in data.content:
             if isinstance(data.content["data"], list):
                 for item in data.content["data"]:
@@ -112,30 +111,20 @@ class SystemDataHandler(YomboLibrary, object):
 
     @inlineCallbacks
     def download_semaphore_process_results_done(self, data):
-        # print("download_semaphore_process_results_donedownload_semaphore_process_results_donedownload_semaphore_process_results_donedownload_semaphore_process_results_done")
-        # print("download_semaphore_process_results_done:")
         if self.bulk_load:
-            # print("doing bulk...db_delete_ids")
-            # print(f"delete ids: {self.db_delete_ids}")
             for table, records in self.db_delete_ids.items():
-                # print(f"delete ids 1, completed IDs: {self.db_completed_ids[table]}")
                 for completed_id in self.db_completed_ids[table]:  # delete items that were not sent to us.
-                    # print("delete ids 2")
                     if completed_id in self.db_existing_ids[table]:
                         del self.db_existing_ids[table][completed_id]
-                # print(f"delete ids a, existingIds: {self.db_existing_ids[table]}")
                 for existing_id in self.db_existing_ids[table]:
-                    # print(f"Left over items: should be deleted.... {table} : {existing_id}")
                     if existing_id not in records:
                         records.append(existing_id)
                 if len(records) > 0:
-                    # print("%s should be delete_many these records: %s" % (table, records))
                     yield self._LocalDB.delete_many(table, records)
             self.db_delete_ids = {}
             self.db_existing_ids = {}
             self.db_completed_ids = {}
 
-            # print(f"doing bulk...db_update_data: {self.db_update_data}")
             for table, records in self.db_update_data.items():
                 if len(records) > 0:
                     save_data = []
@@ -144,30 +133,13 @@ class SystemDataHandler(YomboLibrary, object):
                     yield self._LocalDB.update_many(table, save_data, "id")
             self.db_update_data = {}
 
-            # print(f"Adding new data items: {self.db_insert_data}")
             for table, records in self.db_insert_data.items():
                 if len(records) > 0:
-                    print(f"Adding new data item, table: {table}")
-                    # print(f"Adding new data items, records: {records}")
                     save_data = []
                     for id, data in records.items():
-                        # print(f"Insert data:{table} => {data}")
-                        # yield self._LocalDB.insert(table, data)
-                        # records = yield self._LocalDB.select(
-                        #     table,
-                        #     "*",
-                        #     where=["id = ?", data['id']],
-                        # )
-                        # print(f"db results: {data['id']}: {records}")
                         save_data.append(data)
                     yield self._LocalDB.insert_many(table, save_data)
             self.db_insert_data = {}
-
-        # for table, records in self.db_insert_data.items():
-        #     if len(records) > 0:
-        #         # print("%s should be insert_many these records: %s" % (table, records))
-        #         yield self._LocalDB.insert_many(table, records)
-        # self.db_update_data = {}
 
     def process_incoming(self, data_raw):
         """
@@ -178,80 +150,46 @@ class SystemDataHandler(YomboLibrary, object):
         :return:
         """
         item_type = data_raw["type"]
-        # print(f"process incoming: 001: {item_type}")
-        # print(f"process_incoming: {data_raw}")
-        # print(f"process incoming: 000")
 
         if item_type not in CONFIG_ITEMS:
             logger.warn("Unknown API JSON type: {item_type}  Skipping data!", item_type=item_type)
-            # print(data_raw["attributes"])
             return
 
         config_data = CONFIG_ITEMS[item_type]
-        # print("process incoming: 002")
         current_table = config_data["table"]
-        # print("process incoming: 003")
 
-        # print("05")
-        # current_time = time()
         schema_class = globals()[config_data["schemaclass"]]
-        # print(f"66 {schema_class}")
         schema = schema_class()
-        # print("process incoming: 006")
 
         attributes = self.field_remap(data_raw["attributes"], config_data)
-        # print(f"process_incoming: attributes {attributes}")
 
         try:
-            # print(schema.declared_fields["id"])
-            # print(type(schema.declared_fields["id"]))
-            # print(type(schema.declared_fields["id"]).__name__)
             data = schema.load(attributes)
-            # print("the result of schema:")
-            # print(type(schema))
-            # print(schema)
-            # print(schema.dump())
         except Exception as e:
             logger.info("Loading schema data error for: {item_type}", item_type=item_type)
-            # print(data_raw["attributes"])
             logger.info("Error: {error}", error=e)
             logger.error("---------------==(Traceback)==--------------------------")
             logger.error("{trace}", trace=traceback.format_exc())
             logger.error("--------------------------------------------------------")
             return
-        # print("process incoming: 015")
         if self.bulk_load:
             should_be_deleted = False
-            # print(f"process incoming: 020: {current_table}")
-            # print(f"AA {self.db_existing_ids}")
             if current_table not in self.db_existing_ids:
-                print("process incoming: 020 - creating db_existing_ids current table")
                 self.db_existing_ids[current_table] = []
-            # print("process incoming: 021a")
             if current_table not in self.db_insert_data:
-                # print("process incoming: 021")
                 self.db_insert_data[current_table] = {}
-            # print("process incoming: 022a")
-            # if current_table not in self.db_update_data:
-            #     print("process incoming: 022")
                 self.db_update_data[current_table] = {}
-            # print("process incoming: 023a")
             if current_table not in self.db_delete_ids:
-                # print("process incoming: 023")
                 self.db_delete_ids[current_table] = []
-            # print("process incoming: 024a")
             if current_table not in self.db_completed_ids:
-                # print("process incoming: 024")
                 self.db_completed_ids[current_table] = []
 
-            # print("process incoming: 11")
             if "status" in data and data["status"] == 2:
                 should_be_deleted = True
                 if data["id"] in self.db_existing_ids[current_table] and \
                         data["id"] not in self.db_delete_ids[current_table]:
                     self.db_delete_ids[current_table].append(data["id"])
 
-            # print(f"process incoming: 22 - checking if existing: {current_table} -> {data['id']}")
             if should_be_deleted is False:
                 if data["id"] in self.db_existing_ids[current_table]:
                     # print("ID is in existing data...")
@@ -264,11 +202,8 @@ class SystemDataHandler(YomboLibrary, object):
                     # print(f"New Data: {current_table} -> {data['id']}")
                     # print(f"ID is new data...4 {data}")
                     self.db_insert_data[current_table][data["id"]] = data
-            # print("process incoming: 33")
 
-            # print(f"adding compelted id: {data['id']}")
             if data["id"] not in self.db_completed_ids[current_table]:
-                # print(f"adding compelted id now: {data['id']}")
                 self.db_completed_ids[current_table].append(data["id"])
 
     def field_remap(self, data, config_data):
@@ -285,8 +220,6 @@ class SystemDataHandler(YomboLibrary, object):
             for key, value in data.items():
                 if key in config_data["map"]:
                     new_data[config_data["map"][key]] = value
-                # else:
-                #     new_data[key] = value
             return new_data
         except Exception as e:
             logger.error("---==(Error in field_rempa: {e})==---", e=e)
