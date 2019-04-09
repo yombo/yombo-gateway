7192aaac8cced467732654e47de3f2e275fcf6bf
Mitch Schwenk <mitch@yombo.net>
2019-04-08 17:41:20 -0700

    Changes/Fixes: Many changes. Notably, use the standard render functions. Adjust response headings. Move idempotence to the redner functions.

--- yombo/lib/webinterface/auth.py
+++ yombo/lib/webinterface/auth.py
@@ -1,14 +1,14 @@
 from functools import wraps
 from ratelimit import RateLimitException
-from time import time
 import traceback
 
 from twisted.internet.defer import inlineCallbacks
 
 from yombo.core.exceptions import YomboWarning, YomboNoAccess, YomboInvalidValidation
 from yombo.core.log import get_logger
-from yombo.lib.webinterface.routes.api_v1.__init__ import return_error, args_to_dict
-from yombo.utils import bytes_to_unicode, sha256_compact
+from yombo.lib.webinterface.routes.api_v1 import return_error, args_to_dict
+from yombo.utils import bytes_to_unicode
+from yombo.utils.datatypes import coerce_value
 
 logger = get_logger("library.webinterface.auth")
 
@@ -27,36 +27,18 @@ def update_request(webinterface, request):
     request.auth = None
     request.received_cookies = bytes_to_unicode(request.received_cookies)
     request.args = bytes_to_unicode(request.args)
+    request.setHeader("server", "Apache/2.4.38 (Unix)")
+    request.setHeader("X-Powered-By", "YGW")
     request.setHeader("Cache-Control", "no-cache, no-store, must-revalidate")  # don't cache!
+    # request.setHeader("Access-Control-Allow-Origin", "*")  # For the API, TODO: Make this more restrictive.
+    request.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")  # For the API, TODO: Make this more restrictive.
+                                                    # http://localhost:3000/
+    request.setHeader("Access-Control-Allow-Credentials", "true")  # For the API, TODO: Make this more restrictive.
+
     request.setHeader("Expires", "-1")  # don't cache!
     request.setHeader("X-Frame-Options", "SAMEORIGIN")  # Prevent nesting frames
     request.setHeader("X-Content-Type-Options", "nosniff");  # We"ll do our best to be accurate!
     request.webinterface = webinterface
-
-
-def check_idempotence(webinterface, request, session):
-    """
-    Check if idempotence is in the request and if that key has already been processed for the given user.
-    If the key has already been used, will return a 409 code, otherwise, lets the request continue.
-
-    :param webinterface:
-    :param request:
-    :param session:
-    :return:
-    """
-    idempotence = request.getHeader("x-idempotence")
-    if idempotence is None:
-        arguments = args_to_dict(request.args)
-        idempotence = arguments.get("_idempotence", None)
-    request.idempotence = idempotence
-    if idempotence is not None:
-        idempotence = sha256_compact(f"{session.auth_id}:{idempotence}")
-        if idempotence in webinterface.idempotence:
-            return return_error(request, "idempotence error", 409,
-                                "This idempotence key has already be processed.")
-        webinterface.idempotence[idempotence] = int(time())
-        return True
-    return False
 
 
 def run_first(create_session=None, *args, **kwargs):
@@ -73,24 +55,35 @@ def run_first(create_session=None, *args, **kwargs):
         @wraps(f)
         @inlineCallbacks
         def wrapped_f(webinterface, request, *a, **kw):
-            print("run_first....")
+            # print(f"run_first: f: {f}")
+
+            if "api" in kwargs and kwargs["api"] is True:
+                if webinterface.web_interface_fully_started is False:
+                    # print("auth get session: api request")
+                    return webinterface.render_api_error(request, None,
+                                                         code="booting-2",
+                                                         title="Still loading",
+                                                         detail="Gateway is not ready to process API requests.",
+                                                         response_code=503)
+
             session = None
             update_request(webinterface, request)
             host = request.getHeader("host")
+            hostname = request.getRequestHostname().decode('utf-8')
+
+            # print(f"auth: request.getRequestHostname(): {request.getRequestHostname()}")
             if host is None:
                 logger.info("Discarding request, appears to be malformed host header")
-                return return_need_login(webinterface, request, None, **kwargs)
+                return return_need_login(webinterface, request, None,
+                                         api_message="Malformed request headers.",
+                                         **kwargs)
             host_info = host.split(":")
-            request.requestHeaders.setRawHeaders("host_name", [host_info[0]])
+            request.requestHeaders.setRawHeaders("host_name", [hostname])
 
             if len(host_info) > 1:
                 request.requestHeaders.setRawHeaders("host_port", [host_info[1]])
             else:
                 request.requestHeaders.setRawHeaders("host_port", [None])
-
-            # if hasattr(request, "breadcrumb") is False:
-            #     request.breadcrumb = []
-            #     webinterface.misc_wi_data["breadcrumb"] = request.breadcrumb
 
             try:
                 session = webinterface._AuthKeys.get_session_from_request(request)
@@ -100,16 +93,18 @@ def run_first(create_session=None, *args, **kwargs):
                     session = yield webinterface._WebSessions.get_session_from_request(request)
                     session.touch()
                 except YomboWarning as e:
+                    logger.warn("Error in run_first: {e}", e=e)
                     pass
 
             if session is not None:
-                print("check is valid.. 1")
                 if session.is_valid():
                     request.auth = session
 
             try:
                 if create_session is True and request.auth is None:
+                    # print(f"run_first: before session created: {session}")
                     session = webinterface._WebSessions.create_from_web_request(request)
+                    # print(f"run_first: created new session: {session.asdict()}")
                     request.auth = session
             except RateLimitException as e:
                 logger.warn("Too many sessions being created, stopping this one!")
@@ -142,12 +137,15 @@ def require_auth(roles=None, login_redirect=None, access_platform=None, access_i
         @wraps(f)
         @inlineCallbacks
         def wrapped_f(webinterface, request, *a, **kw):
+            # print(f"require_auth: f: {f}")
             update_request(webinterface, request)
 
             host = request.getHeader("host")
             if host is None:
                 logger.info("Discarding request, appears to be malformed host header")
-                return return_need_login(webinterface, request, None, **kwargs)
+                return return_need_login(webinterface, request, None,
+                                         api_message="Malformed request headers.",
+                                         **kwargs)
             host_info = host.split(":")
             request.requestHeaders.setRawHeaders("host_name", [host_info[0]])
 
@@ -162,7 +160,14 @@ def require_auth(roles=None, login_redirect=None, access_platform=None, access_i
 
             # Try to find a session id from the client.
             if "api" in kwargs and kwargs["api"] is True:
-                print("auth get session: api request")
+                if webinterface.web_interface_fully_started is False:
+                    # print("auth get session: api request")
+                    return webinterface.render_api_error(request, None,
+                                                         code="booting-1",
+                                                         title="Still loading",
+                                                         detail="Gateway is not ready to process API requests.",
+                                                         response_code=503)
+
                 try:
                     session = webinterface._AuthKeys.get_session_from_request(request)
                     session.touch()
@@ -172,31 +177,35 @@ def require_auth(roles=None, login_redirect=None, access_platform=None, access_i
                         session = yield webinterface._WebSessions.get_session_from_request(request)
                     except YomboWarning as e:
                         logger.info("API request doesn't have a valid auth key or session. Bye bye: {e}", e=e)
-                        return return_need_login(webinterface, request, None, **kwargs)
-                    results = check_idempotence(webinterface, request, session)
+                        return return_need_login(webinterface, request, None,
+                                                 api_message="API request doesn't have a valid auth key or session.",
+                                                 **kwargs)
+                    results = webinterface.check_idempotence(request, session)
                     if isinstance(results, bool) is False:
                         return results
             else:
-                print("auth get session: web request")
+                # print("auth get session: web request")
                 try:
                     session = yield webinterface._WebSessions.get_session_from_request(request)
                 except YomboWarning as e:
-                    session = setup_login_redirect(webinterface, request, None, login_redirect)
+                    session = webinterface._WebSessions.create_from_web_request(request)
+                    # print(f"require_auth: created new session: {session.asdict()}")
+                    setup_login_redirect(webinterface, request, session, login_redirect)
                     logger.debug("Discarding request, api requests not accepted: {e}", e=e)
                     logger.debug("Request: {request}", request=request)
-                    return return_need_login(webinterface, request, None, **kwargs)
+                    return return_need_login(webinterface, request, None,
+                                             api_message=f"Discarding request, api requests not accepted: {e}",
+                                             **kwargs)
 
             # print(f"auth hh. session: {session}")
             # Now validate if the session is any good.
             # print(f"auth session.enabled: {session.enabled}")
             # print(f"auth session.is_valid: {session.is_valid()}")
-            print("check is valid.. 3")
+            # print("check is valid.. 3")
 
             if session.enabled is False or session.is_valid() is False or session.has_user is False:
                 # print("auth: require_auth: session is not enable or is not valid!")
-                return return_need_login(webinterface,
-                                         request,
-                                         session,
+                return return_need_login(webinterface, request, session,
                                          api_message="API Key is not valid.",
                                          **kwargs)
 
@@ -205,7 +214,7 @@ def require_auth(roles=None, login_redirect=None, access_platform=None, access_i
 
             if access_platform is not None and access_item is not None and access_action is not None:
                 if session.has_access(access_platform, access_item, access_action, raise_error=False) is False:
-                    return return_no_access(webinterface, request, "No access", **kwargs)
+                    return return_no_access(webinterface, request, session, "No access", **kwargs)
                     pass
 
             results = yield auth_run_wrapped_function(f, webinterface, request, session, *a, **kw)
@@ -234,11 +243,12 @@ def auth_run_wrapped_function(f, webinterface, request, session, *a, **kw):
         results = yield call(f, webinterface, request, session, *a, **kw)
         return results
     except YomboInvalidValidation as e:
-        return return_not_valid_input(webinterface, request, **kw)
+        return return_not_valid_input(webinterface, request, session, **kw)
     except YomboNoAccess as e:
-        return return_no_access(webinterface, request, "No access", **kw)
+        return return_no_access(webinterface, request, session, "No access", **kw)
     except Exception as e:  # catch anything here...so can display details.
         logger.error("---------------==(Traceback)==--------------------------")
+        logger.error("Exception: {e}", e=e)
         logger.error("Function: {f}", f=f)
         logger.error("Request: {request}", request=request)
         logger.error("{trace}", trace=traceback.format_exc())
@@ -249,9 +259,15 @@ def auth_run_wrapped_function(f, webinterface, request, session, *a, **kw):
         else:
             content_type = ""
         if "json" in content_type:
-            return return_error(request, "Server Error", 500, traceback.format_exc())
-        page = webinterface.get_template(request, webinterface.wi_dir + "/pages/misc/traceback.html")
-        return page.render(traceback=traceback.format_exc())
+            return webinterface.render_api_error(request, None,
+                                                 code="server_error",
+                                                 title="Server Error",
+                                                 detail="The server encountered an error while processing this request.",
+                                                 response_code=500)
+        return webinterface.render(request, session,
+                                   webinterface.wi_dir + "/pages/errors/traceback.html",
+                                   traceback=traceback.format_exc()
+                                   )
 
 
 def setup_login_redirect(webinterface, request, session, login_redirect):
@@ -264,22 +280,39 @@ def setup_login_redirect(webinterface, request, session, login_redirect):
     :param login_redirect:
     :return:
     """
+    uri = request.uri.decode('utf-8')
+    # print("setup_login_redirect: ###################################################################################3")
     if login_redirect is None:  # only create a new session if we need too
-        return
+        login_redirect = request.uri.decode('utf-8')
 
+    # print(f"setup_login_redirect: login_redirect: {login_redirect}")
+    # print(f"setup_login_redirect: has session: {session}")
     if session is None:
         try:
             session = webinterface._WebSessions.create_from_web_request(request)
+            # print(f"setup_login_redirect: created new session: {session.asdict()}")
         except YomboWarning as e:
             logger.warn("Discarding request, appears to be malformed request. Unable to create session.")
-            return return_need_login(webinterface, request, None)
+            return return_need_login(webinterface, request, None,
+                                     api_message="Malformed request headers.",
+                                     )
         except RateLimitException as e:
-            logger.warn("Too sessions being created!")
+            logger.warn("Too many sessions being created!")
             return _("ui::messages::rate_limit_exceeded", "Too many attempts, try again later.")
+
+    # print(f"setup_login_redirect: gotz a session....{request.args.get('autologinredirect', [0])[0]}")
+
+    try:
+        auto_login_redirect = coerce_value(request.args.get('autologinredirect', [0])[0], 'int')
+    except:
+        auto_login_redirect = 0
+    # print(f"setup_login_redirect: auto_login_redirect_input: {auto_login_redirect}")
 
     session.created_by = "login_redirect"
     request.received_cookies[webinterface._WebSessions.config.cookie_session_name] = session.auth_id
     session["login_redirect"] = login_redirect
+    session["auto_login_redirect"] = auto_login_redirect
+    # print(f"auth: setup_login_redirect, session: {session.asdict()}")
     return session
 
 
@@ -300,11 +333,17 @@ def return_need_login(webinterface, request, session, api_message=None, **kwargs
     else:
         content_type = ""
     if ("api" in kwargs and kwargs["api"] is True) or "json" in content_type:
-        return return_error(request, "Unauthorized", 401, api_message)
+        if api_message is None:
+            api_message = "The request requires authorization, none was provided."
+        return webinterface.render_api_error(request, None,
+                                             code="unauthorized",
+                                             title="Unauthorized",
+                                             detail=api_message,
+                                             response_code=401)
     return webinterface.redirect(request, "/user/login")
 
 
-def return_not_valid_input(webinterface, request, **kwargs):
+def return_not_valid_input(webinterface, request, session, **kwargs):
     """
     The system caught a YomboInvalidValidation exception and redirected here. Typically when a user
     submits bogus arguments.
@@ -322,16 +361,19 @@ def return_not_valid_input(webinterface, request, **kwargs):
     else:
         content_type = ""
     if ("api" in kwargs and kwargs["api"] is True) or "json" in content_type:
-        return return_error(request, "Forbidden - No access to protected resource", 400)
-    page = webinterface.get_template(request, webinterface.wi_dir + "/pages/errors/400.html")
-    return page.render(
-        alerts=webinterface.get_alerts(),
-        title="Invalid request",
-        message="The request being made is invalid.",
-    )
+        return webinterface.render_api_error(request, None,
+                                             code="invalid_request",
+                                             title="Invalid request",
+                                             detail="The request was invalid.",
+                                             response_code=400)
+    return webinterface.render(request, session,
+                               webinterface.wi_dir + "/pages/errors/400.html",
+                               title="Invalid request",
+                               message="The request being made is invalid.",
+                               )
 
 
-def return_no_access(webinterface, request, error, **kwargs):
+def return_no_access(webinterface, request, session, error, **kwargs):
     """
     Returns the 403 page to user.
 
@@ -347,6 +389,9 @@ def return_no_access(webinterface, request, error, **kwargs):
     else:
         content_type = ""
     if ("api" in kwargs and kwargs["api"] is True) or "json" in content_type:
-        return return_error(request, "Forbidden - No access to protected resource", 403)
-    page = webinterface.get_template(request, webinterface.wi_dir + "/pages/errors/403.html")
-    return page.render(alerts=webinterface.get_alerts(), error=error)
+        return webinterface.render_api_error(request, None,
+                                             code="forbidden",
+                                             title="Forbidden",
+                                             detail="No access to the protected resource.",
+                                             response_code=403)
+    return webinterface.render(request, session, webinterface.wi_dir + "/pages/errors/403.html", error=error)
