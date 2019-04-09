d2d5c9ac2f0fbb6e426295a7dfefc935e7031671
Mitch Schwenk <mitch@yombo.net>
2019-04-09 00:14:23 -0700

    Change: Api/v1/server routes to use the new renderers.

--- yombo/lib/webinterface/routes/api_v1/server.py
+++ yombo/lib/webinterface/routes/api_v1/server.py
@@ -1,3 +1,13 @@
+"""
+Handles items for the basic/static webinterface, NOT for the frontend.
+
+.. moduleauthor:: Mitch Schwenk <mitch-gw@yombo.net>
+
+:copyright: Copyright 2018-2019 by Yombo.
+:license: LICENSE for details.
+:view-source: `View Source Code <https://yombo.net/Docs/gateway/html/current/_modules/yombo/lib/cache.html>`_
+"""
+
 # Import python libraries
 import json
 
@@ -12,164 +22,17 @@ from yombo.constants import CONTENT_TYPE_JSON
 def route_api_v1_server(webapp):
     with webapp.subroute("/api/v1") as webapp:
 
-        @webapp.route("/server/commands/index", methods=["GET"])
-        @require_auth(api=True)
-        @inlineCallbacks
-        def apiv1_server_commands_index(webinterface, request, session):
-            try:
-                offset = request.args.get("offset")[0]
-            except:
-                offset = 0
-            try:
-                limit = request.args.get("limit")[0]
-            except:
-                limit = 50
-            try:
-                search = request.args.get("search")[0]
-            except:
-                search = None
-
-            url = f"/v1/command?_pagestart={offset}&_pagelimit={limit}"
-            if search is not None:
-                url = url + f"&?_filters[label]*{search}&_filters[description]*{search}" \
-                            f"&_filters[machine_label]*{search}&_filteroperator=or"
-
-            try:
-                results = yield webinterface._YomboAPI.request("GET", url, session=session["yomboapi_session"])
-            except YomboWarning as e:
-                return return_error(request, e.message, e.errorno)
-
-            data = {
-                "total": results["content"]["pages"]["total_items"],
-                "rows": results["data"],
-            }
-            request.setHeader("Content-Type", CONTENT_TYPE_JSON)
-            return json.dumps(data)
-
         @webapp.route("/server/dns/check_available/<string:dnsname>", methods=["GET"])
         @require_auth(api=True)
         @inlineCallbacks
         def apiv1_server_dns_check_available(webinterface, request, session, dnsname):
-
-            url = f"/v1/dns/check_available/{dnsname}"
-
+            url = f"/v1/dns_domains/check_available/{dnsname}"
+            auth_header = yield session.authorization_header()
             try:
-                results = yield webinterface._YomboAPI.request("GET", url, session=session["yomboapi_session"])
+                response = yield webinterface._YomboAPI.request("GET", url, authorization_header=auth_header)
             except YomboWarning as e:
                 return return_error(request, e.message, e.errorno)
 
-            request.setHeader("Content-Type", CONTENT_TYPE_JSON)
-            return json.dumps(results["data"])
-
-        @webapp.route("/server/devicetypes/index", methods=["GET"])
-        @require_auth(api=True)
-        @inlineCallbacks
-        def apiv1_server_devicetypes_index(webinterface, request, session):
-            try:
-                offset = request.args.get("offset")[0]
-            except:
-                offset = 0
-            try:
-                limit = request.args.get("limit")[0]
-            except:
-                limit = 50
-            try:
-                search = request.args.get("search")[0]
-            except:
-                search = None
-
-            url = f"/v1/device_type?_pagestart={offset}&_pagelimit={limit}"
-            if search is not None:
-                url = url + f"&?_filters[label]*{search}&_filters[description]*{search}" \
-                            f"&_filters[machine_label]*{search}&_filteroperator=or"
-
-            try:
-                results = yield webinterface._YomboAPI.request("GET", url, session=session["yomboapi_session"])
-            except YomboWarning as e:
-                return return_error(request, e.message, e.errorno)
-
-            data = {
-                "total": results["content"]["pages"]["total_items"],
-                "rows": results["data"],
-            }
-            request.setHeader("Content-Type", CONTENT_TYPE_JSON)
-            return json.dumps(data)
-
-        @webapp.route("/server/input_type/index", methods=["GET"])
-        @require_auth(api=True)
-        @inlineCallbacks
-        def apiv1_server_inputtypes_index(webinterface, request, session):
-            try:
-                offset = request.args.get("offset")[0]
-            except:
-                offset = 0
-            try:
-                limit = request.args.get("limit")[0]
-            except:
-                limit = 50
-            try:
-                search = request.args.get("search")[0]
-            except:
-                search = None
-
-            url = f"/v1/input_type?_pagestart={offset}&_pagelimit={limit}"
-            if search is not None:
-                url = url + f"&?_filters[label]*{search}&_filters[description]*{search}" \
-                            f"&_filters[machine_label]*{search}&_filteroperator=or"
-
-            try:
-                results = yield webinterface._YomboAPI.request("GET", url, session=session["yomboapi_session"])
-            except YomboWarning as e:
-                return return_error(request, e.message, e.errorno)
-            data = {
-                "total": results["content"]["pages"]["total_items"],
-                "rows": results["data"],
-            }
-            request.setHeader("Content-Type", CONTENT_TYPE_JSON)
-            return json.dumps(data)
-
-        @webapp.route("/server/modules/index", methods=["GET"])
-        @require_auth(api=True)
-        @inlineCallbacks
-        def apiv1_server_modules_index(webinterface, request, session):
-            try:
-                offset = request.args.get("offset")[0]
-            except:
-                offset = 0
-            try:
-                limit = request.args.get("limit")[0]
-            except:
-                limit = 50
-            try:
-                search = request.args.get("search")[0]
-            except:
-                search = None
-
-            url = f"/v1/module?_pagestart={offset}&_pagelimit={limit}"
-            if search is not None:
-                url = url + f"&?_filters[label]*{search}&_filters[short_description]*{search}" \
-                            f"&_filters[machine_label]*{search}&_filteroperator=or"
-
-            try:
-                results = yield webinterface._YomboAPI.request("GET", url, session=session["yomboapi_session"])
-            except YomboWarning as e:
-                return return_error(request, e.message, e.errorno)
-            data = {
-                "total": results["content"]["pages"]["total_items"],
-                "rows": results["data"],
-            }
-            request.setHeader("Content-Type", CONTENT_TYPE_JSON)
-            return json.dumps(data)
-
-        @webapp.route("/server/modules/show/<string:module_id>", methods=["GET"])
-        @require_auth(api=True)
-        @inlineCallbacks
-        def apiv1_server_modules_show_one(webinterface, request, session, module_id):
-            try:
-                results = yield webinterface._YomboAPI.request("GET", f"/v1/module/{module_id}",
-                                                               session=session["yomboapi_session"])
-            except YomboWarning as e:
-                return return_error(request, e.message, e.errorno)
-
-            request.setHeader("Content-Type", CONTENT_TYPE_JSON)
-            return json.dumps(results["data"])
+            return webinterface.render_api(request, session,
+                                           data=response.content["data"]
+                                           )
