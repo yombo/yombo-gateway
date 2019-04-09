c518c934d51f2c293ec7b0eb1f80f071d57c1a6f
Mitch Schwenk <mitch@yombo.net>
2019-04-09 00:15:11 -0700

    Change: api/v1/system to use new renders and JSON API format.

--- yombo/lib/webinterface/routes/api_v1/system.py
+++ yombo/lib/webinterface/routes/api_v1/system.py
@@ -1,38 +1,53 @@
 # Import python libraries
 from time import time
 
-from yombo.lib.webinterface.auth import require_auth, run_first
-from yombo.lib.webinterface.routes.api_v1.__init__ import return_good, return_error
+from yombo.constants import VERSION
+from yombo.lib.webinterface.auth import require_auth
 from yombo.utils import random_string
 
 
 def route_api_v1_system(webapp):
     with webapp.subroute("/api/v1") as webapp:
 
-        @webapp.route("/tools/awake")
-        def apiv1_system_tools_awake(webinterface, request):
-            request.setHeader("Access-Control-Allow-Origin", "*");
-            return 1
-
-        @webapp.route("/tools/ping")
+        @webapp.route("/system/info", methods=["GET"])
         @require_auth(api=True)
-        def apiv1_system_tools_ping(webinterface, request):
-            try:
-                request_id = request.args.get("id")[0]
-            except Exception as e:
-                request_id = random_string(length=12)
+        def apiv1_system_info(webinterface, request, session):
+            gateway = webinterface._Gateways.local
+            attributes = {**gateway.asdict(), **{
+                             "gateway_id": str(webinterface._Gateways.local_id),
+                             "operating_mode": str(webinterface._Loader.operating_mode)
+                            }
+                          }
 
-            return return_good(request,
-                               payload={"id": request_id,
-                                        "time": float(time()),
-                                        }
-                               )
+            attributes["running_since"] = int(webinterface._Atoms["running_since"])
+            attributes["uptime"] = int(time() - attributes["running_since"])
+            attributes["version"] = VERSION
+            return webinterface.render_api(request, session,
+                                           data_type="system_info",
+                                           id=gateway.gateway_id,
+                                           attributes=attributes,
+                                           )
 
-        @webapp.route("/tools/uptime")
-        @run_first()
-        def apiv1_system_tools_uptime(webinterface, request, session):
+        @webapp.route("/system/awake", methods=["GET"])
+        def apiv1_system_tools_awake(webinterface, request):
             if webinterface.web_interface_fully_started is False:
-                return return_error(request, message="Not ready yet.")
+                # print("auth get session: api request")
+                return webinterface.render_api_error(request, None,
+                                                     code="booting-other",
+                                                     title="Still loading",
+                                                     detail="Gateway is not ready to process API requests.",
+                                                     response_code=503)
+
+            request.setHeader("Access-Control-Allow-Origin", "*")
+            return webinterface.render_api(request, None,
+                                           data_type="system_awake",
+                                           id=int(webinterface._Atoms["running_since"]),
+                                           attributes={"id": int(webinterface._Atoms["running_since"])},
+                                           )
+
+        @webapp.route("/system/uptime", methods=["GET"])
+        @require_auth(api=True)
+        def apiv1_system_status_uptime(webinterface, request, session):
             try:
                 timeonly = str(request.args.get("timeonly")[0])
                 if timeonly == "1":
@@ -40,8 +55,25 @@ def route_api_v1_system(webapp):
             except Exception as e:
                 pass
 
-            return return_good(request,
-                               payload={
-                                        "time": str(webinterface._Atoms["running_since"]),
-                                        }
-                               )
+            return webinterface.render_api(request, session,
+                                           data_type="system_uptime",
+                                           attributes={
+                                               "start_time": str(webinterface._Atoms["running_since"])
+                                               }
+                                           )
+
+        @webapp.route("/system/ping", methods=["GET"])
+        @require_auth(api=True)
+        def apiv1_system_tools_ping(webinterface, request, session):
+            try:
+                request_id = request.args.get("id")[0]
+            except Exception as e:
+                request_id = random_string(length=12)
+
+            return webinterface.render_api(request, session,
+                                           data_type="system_ping",
+                                           attributes={
+                                               "id": request_id,
+                                               "time": float(time()),
+                                               }
+                                           )
