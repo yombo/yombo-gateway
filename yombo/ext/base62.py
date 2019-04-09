19ce24e15ab3173e82490568c63596bf11c9e94b
Mitch Schwenk <mitch@yombo.net>
2019-04-08 21:40:31 -0700

    Update: Base62 3rd party extension updated.

--- yombo/ext/base62.py
+++ yombo/ext/base62.py
@@ -2,45 +2,22 @@
 """
 base62
 ~~~~~~
-https://github.com/suminb/base62/blob/develop/base62.py
 
-Copyright (c) 2014, Sumin Byeon
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies,
-either expressed or implied, of the FreeBSD Project.
-
-This version has been modified for Yombo to use the correct charset order.
+Originated from http://blog.suminb.com/archives/558
 """
 
 __title__ = 'base62'
 __author__ = 'Sumin Byeon'
 __email__ = 'suminb@gmail.com'
-__version__ = '0.3.2'
+__version__ = '0.4.0'
 
-CHARSET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 BASE = 62
+CHARSET_DEFAULT = (
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+)
+CHARSET_INVERTED = (
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+)
 
 
 def bytes_to_int(s, byteorder='big', signed=False):
@@ -64,7 +41,7 @@ def bytes_to_int(s, byteorder='big', signed=False):
         return sum(ds)
 
 
-def encode(n, minlen=1):
+def encode(n, minlen=1, charset=CHARSET_DEFAULT):
     """Encodes a given integer ``n``."""
 
     chs = []
@@ -72,7 +49,7 @@ def encode(n, minlen=1):
         r = n % BASE
         n //= BASE
 
-        chs.append(CHARSET[r])
+        chs.append(charset[r])
 
     if len(chs) > 0:
         chs.reverse()
@@ -80,21 +57,21 @@ def encode(n, minlen=1):
         chs.append('0')
 
     s = ''.join(chs)
-    s = CHARSET[0] * max(minlen - len(s), 0) + s
+    s = charset[0] * max(minlen - len(s), 0) + s
     return s
 
 
-def encodebytes(s):
+def encodebytes(s, charset=CHARSET_DEFAULT):
     """Encodes a bytestring into a base62 string.
 
     :param s: A byte array
     """
 
     _check_bytes_type(s)
-    return encode(bytes_to_int(s))
+    return encode(bytes_to_int(s), charset=charset)
 
 
-def decode(b):
+def decode(b, charset=CHARSET_DEFAULT):
     """Decodes a base62 encoded value ``b``."""
 
     if b.startswith('0z'):
@@ -102,20 +79,20 @@ def decode(b):
 
     l, i, v = len(b), 0, 0
     for x in b:
-        v += _value(x) * (BASE ** (l - (i + 1)))
+        v += _value(x, charset=charset) * (BASE ** (l - (i + 1)))
         i += 1
 
     return v
 
 
-def decodebytes(s):
+def decodebytes(s, charset=CHARSET_DEFAULT):
     """Decodes a string of base62 data into a bytes object.
 
     :param s: A string to be decoded in base62
     :rtype: bytes
     """
 
-    decoded = decode(s)
+    decoded = decode(s, charset=charset)
     buf = bytearray()
     while decoded > 0:
         buf.append(decoded & 0xff)
@@ -125,11 +102,11 @@ def decodebytes(s):
     return bytes(buf)
 
 
-def _value(ch):
+def _value(ch, charset):
     """Decodes an individual digit of a base62 encoded string."""
 
     try:
-        return CHARSET.index(ch)
+        return charset.index(ch)
     except ValueError:
         raise ValueError('base62: Invalid character (%s)' % ch)
 
