b7580a04f4d47872f7c0d3d875a7759b743b37aa
Mitch Schwenk <mitch@yombo.net>
2019-05-08 11:45:06 -0700

    Change (FE): Persist is now debounced.
    VUEX persist was killing the responsiveness of the client due to constantly
    saving data to local storage. lodash debounce is now used to help control
    how many times data is saved. Now, data will only be saved 2 seconds after
    the last sync.

--- yombo/frontend/plugins/localStorage.js
+++ yombo/frontend/plugins/localStorage.js
@@ -1,5 +1,7 @@
 import createPersistedState from 'vuex-persistedstate'
+// import createPersistedState from './persistdata'
 import LZString from "lz-string";
+const debounce = require('lodash/debounce');
 
 export default ({store}) => {
   createPersistedState({
@@ -8,48 +10,71 @@ export default ({store}) => {
         [
           'yombo',
         ],
+      setState: debounce((key, state, storage) => {
+          storage.setItem(key, JSON.stringify(state));
+          }, 2000, { 'leading': false, 'trailing': true }
+        ),
       storage: {
-        getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
-        setItem: (key, value) => localStorage.setItem(key, LZString.compressToUTF16(value)),
-        removeItem: key => localStorage.removeItem(key),
-        }
-      }
-  )(store);
+          getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
+          setItem: function (key, value) {
+            localStorage.setItem(key, LZString.compressToUTF16(value));
+            },
+          removeItem: key => localStorage.removeItem(key),
+          }
+      })(store);
   createPersistedState({
       key: 'ybofa_gwapi',
       paths:
         [
           'gateway',
         ],
+      setState: debounce((key, state, storage) => {
+          storage.setItem(key, JSON.stringify(state));
+          }, 2000, { 'leading': false, 'trailing': true }
+        ),
       storage: {
-        getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
-        setItem: (key, value) => localStorage.setItem(key, LZString.compressToUTF16(value)),
-        removeItem: key => localStorage.removeItem(key),
-        }
-      }
-  )(store);
+          getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
+          setItem: function (key, value) {
+            localStorage.setItem(key, LZString.compressToUTF16(value));
+            },
+          removeItem: key => localStorage.removeItem(key),
+          }
+      })(store);
   createPersistedState({
-    key: 'ybo_fa_orm',
-    paths:
-      [
-        'entities'
-      ],
-    storage: {
-        getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
-        setItem: function (key, value) { localStorage.setItem(key, LZString.compressToUTF16(value)) },
-        removeItem: key => localStorage.removeItem(key),
-        }
-    })(store);
-  // createPersistedState({
-  //   key: 'ybo_fa_modules',
-  //   storage: {
-  //       getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
-  //       setItem: function (key, value) {
-  //         if (key.startsWith('modules_')) {
-  //           localStorage.setItem(key, LZString.compressToUTF16(value))
-  //         }
-  //       },
-  //       removeItem: key => localStorage.removeItem(key),
-  //       }
-  //   })(store);
+      key: 'ybo_fa_orm',
+      paths:
+        [
+          'entities'
+        ],
+      setState: debounce((key, state, storage) => {
+          storage.setItem(key, JSON.stringify(state));
+          }, 4000, { 'leading': false, 'trailing': true }
+        ),
+      storage: {
+          getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
+          setItem: function (key, value) {
+            localStorage.setItem(key, LZString.compressToUTF16(value));
+            },
+          removeItem: key => localStorage.removeItem(key),
+          }
+      })(store);
+
+  createPersistedState({
+      key: 'ybo_fa_modules',
+      paths:
+        [
+          'modules'
+        ],
+      setState: debounce((key, state, storage) => {
+          storage.setItem(key, JSON.stringify(state));
+          }, 2000, { 'leading': false, 'trailing': true }
+        ),
+      storage: {
+          getItem: key => LZString.decompressFromUTF16(localStorage.getItem(key)),
+          setItem: function (key, value) {
+            localStorage.setItem(key, LZString.compressToUTF16(value));
+            },
+          removeItem: key => localStorage.removeItem(key),
+          }
+      })(store);
 }
