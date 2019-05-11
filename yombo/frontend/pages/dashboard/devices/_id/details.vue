80195000db6d32f2ac31562f7a10a84d56d8b762
Mitch Schwenk <mitch@yombo.net>
2019-05-10 23:30:05 -0700

    New/Changes (FE): Add basic details for devices.

--- yombo/frontend/pages/dashboard/devices/_id/details.vue
+++ yombo/frontend/pages/dashboard/devices/_id/details.vue
@@ -4,128 +4,155 @@
       <card card-body-classes="table-full-width">
         <div slot="header">
         <h4 class="card-title">
-           {{ $t('ui.navigations.devices') }}
-         </h4>
+          {{ $t('ui.common.device') }}: {{item.full_label}}
+          <actions path="dashboard-devices" :id="id"
+                   edit
+                   enabledispatch="yombo/devices/enable"
+                   disabledispatch="yombo/devices/disable"
+                   deletedispatch="yombo/devices/delete"
+          ></actions>
+        </h4>
         </div>
         <div class="card-body">
           {{ $t('ui.common.device')}}: {{id}}
+          <p>{{item}}</p>
+
+          <template v-if="item.status == 0">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default panel-red">
+                        <div class="panel-heading">
+                            <label>Device Disabled</label>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <label style="margin-top: 0px; margin-bottom: 0px">This device has been disabled and is not accessible to the
+                            system for automation purposes.</label>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+          </template>
+          <template v-if="item.status == 2">
+              <div class="row">
+                  <div class="col-lg-12">
+                      <div class="panel panel-default panel-red">
+                          <div class="panel-heading">
+                              <label>Device Deleted</label>
+                          </div>
+                          <!-- /.panel-heading -->
+                          <div class="panel-body">
+                              <label style="margin-top: 0px; margin-bottom: 0px">This device has been deleted and is not accessible to the
+                              system for automation purposes.</label>
+                          </div>
+                      </div>
+                  </div>
+                  <!-- /.col-lg-12 -->
+              </div>
+          </template>
+          <div class="row">
+            <div class="col-lg-12">
+              <b-tabs card content-class="">
+                <b-tab title="Details" active>
+                  <div class="col-lg-6 col-md-6">
+                      <!-- /.panel-heading -->
+                          <label class="detail-label-first">Gateway: </label><br>
+                          {{ gateway.label }} <br>
+                          <label class="detail-label">Label: </label><br>
+                          {{ item.label }} <br>
+                          <i>Effective:</i> {{ location(item.area_id).label }} {{item.label}}<br>
+                          <label class="detail-label">Machine Label: </label><br>
+                          {{ item.machine_label }}<br>
+                          <label class="detail-label">Location: </label><br>
+                          {{ location(item.location_id).label }} -> {{ location(item.area_id).label }}<br>
+                          <label class="detail-label">Description: </label><br>
+                          {{ item.description }}<br>
+                          <label class="detail-label">Status:</label><br>
+                          {{ item.status }}<br>
+                          <label class="detail-label">Pin Required // Pin Code: </label><br>
+                          {{ item.pin_required|yes_no }} // {{ item.pin_code }} <br>
+                          <label class="detail-label">Device Type: </label><br>
+
+                          <label class="detail-label">Controllable: </label><br>
+                          {{ item.is_controllable|yes_no }}<br>
+                          <label class="detail-label">Allow Direct Control: </label><br>
+                          {{ item.is_direct_controllable|yes_no }}<br>
+                          <label class="detail-label">Allowed in scenes: </label><br>
+                          {{ item.is_allowed_in_scenes|yes_no }}<br>
+                          <label class="detail-label">Statistic Type: </label><br>
+                          {{ item.statistic_type }}<br>
+                          <label class="detail-label">Statistic Label: </label><br>
+                          {{ item.statistic_label_slug }}<br>
+                          <label class="detail-label">Statistic Bucket Size: </label><br>
+                          {{ item.statistic_bucket_size }}<br>
+                          <label class="detail-label">Statistic Lifetime: </label><br>
+                          {{ item.statistic_lifetime }}<br>
+                    <!-- /.panel-body -->
+                  </div>
+                </b-tab>
+                <b-tab title="History">
+                  <p>I'm the second tab</p>
+                </b-tab>
+                <b-tab title="Permissions">
+                  <p>I'm a disabled tab!</p>
+                </b-tab>
+                <b-tab title="Variables">
+                  <p>I'm a disabled tab!</p>
+                </b-tab>
+                <b-tab title="Debug">
+                  <p>I'm a disabled tab!</p>
+                </b-tab>
+              </b-tabs>
+            </div>
+          </div>
         </div>
-        <p>{{item}}</p>
       </card>
     </div>
 
   </div>
 </template>
 <script>
+import { Actions } from '@/components/Dashboard/Actions';
+import { ActionDelete, ActionDetails, ActionDisable, ActionEdit, ActionEnable } from '@/components/Dashboard/Actions';
+
 import { Table, TableColumn } from 'element-ui';
-import _ from 'lodash';
+
+import Device from '@/models/device'
+import Gateway from '@/models/gateway'
+import Location from '@/models/location'
 
 export default {
   layout: 'dashboard',
   components: {
     [Table.name]: Table,
-    [TableColumn.name]: TableColumn
+    [TableColumn.name]: TableColumn,
+    Actions,
+    ActionEdit,
   },
   data() {
     return {
       id: this.$route.params.id,
+      gateway: {},
     };
   },
   computed: {
     item () {
-      return this.$store.state.gateway.device_commands.data[this.id];
+      return Device.query().where('id', this.id).first()
     },
   },
-
   methods: {
-    handleDelete(index, row) {
-      this.$swal({
-        title: 'Are you sure?',
-        text: `You won't may not be able to revert this!`,
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn-success btn-fill',
-        cancelButtonClass: 'btn btn-danger btn-fill',
-        confirmButtonText: 'Yes, delete it!',
-        buttonsStyling: false
-      }).then(result => {
-        if (result.value) {
-          this.$store.dispatch('devices/delete', row.id);
-          this.$swal({
-            title: 'Deleted!',
-            text: `You deleted ${row.full_name}`,
-            type: 'success',
-            confirmButtonClass: 'btn btn-success btn-fill',
-            buttonsStyling: false
-          });
-        }
-      });
-    },
-    async handleEnable(index, row) {
-      await this.$swal({
-        title: 'Are you sure?',
-        text: `This will enable the device, the gateway may need to be rebooted.`,
-        type: 'info',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn-success btn-fill',
-        cancelButtonClass: 'btn btn-danger btn-fill',
-        confirmButtonText: 'Yes, enable it!',
-        buttonsStyling: false
-      }).then(result => {
-        if (result.value) {
-          let results = this.$store.dispatch('devices/enable', row.id);
-          console.log('Enabled status: ' + JSON.stringify(results))
-          this.$swal({
-            title: 'Enabled!',
-            text: `You enabled ${row.full_name}`,
-            type: 'success',
-            confirmButtonClass: 'btn btn-success btn-fill',
-            buttonsStyling: false
-          });
-        }
-      });
-    },
-    async handleDisable(index, row) {
-      await this.$swal({
-        title: 'Are you sure?',
-        text: `This will disable the device, the gateway may need to be rebooted.`,
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn-success btn-fill',
-        cancelButtonClass: 'btn btn-danger btn-fill',
-        confirmButtonText: 'Yes, enable it!',
-        buttonsStyling: false
-      }).then(result => {
-        if (result.value) {
-          let results = this.$store.dispatch('devices/disable', row.id);
-          console.log('Enabled status: ' + JSON.stringify(results))
-          this.$swal({
-            title: 'Enabled!',
-            text: `You enabled ${row.full_name}`,
-            type: 'success',
-            confirmButtonClass: 'btn btn-success btn-fill',
-            buttonsStyling: false
-          });
-        }
-      });
-    },
-
-
-    tableRowClassName({ rowIndex }) {
-      if (rowIndex === 0) {
-        return 'table-success';
-      } else if (rowIndex === 2) {
-        return 'table-info';
-      } else if (rowIndex === 4) {
-        return 'table-danger';
-      } else if (rowIndex === 6) {
-        return 'table-warning';
-      }
-      return '';
-    },
+    location: function(id) {
+      console.log("location id: " + id);
+      console.log(Location.query().where('id', id).first());
+      return Location.query().where('id', id).first();
+    }
   },
   mounted () {
-    // console.log("devices mounted....");
+    this.$store.dispatch('yombo/devices/refresh');
+    this.$store.dispatch('yombo/locations/fetch');
+
+    this.gateway = Gateway.query().where('id', this.item.gateway_id).first()
   },
   beforeDestroy () {
     // console.log("device before destroy")
@@ -133,8 +160,19 @@ export default {
 };
 </script>
 
-<style>
-/*.table-transparent {*/
-/*  background-color: transparent !important;*/
-/*}*/
+<style lang="less" scoped>
+  .h4 {
+    margin-top: 5px !important;
+  }
+  .detail-label {
+    color: black !important;
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 15px;
+    margin-bottom: 0px
+  }
+  .detail-label-first {
+    .detail-label;
+    margin-top: 0px;
+  }
 </style>
--- yombo/frontend/pages/dashboard/devices/_id/edit.vue
+++ yombo/frontend/pages/dashboard/devices/_id/edit.vue
@@ -4,7 +4,7 @@
       <card card-body-classes="table-full-width">
         <div slot="header">
         <h4 class="card-title">
-           {{ $t('ui.common.edit_device') }}: {{item.full_label}}
+           {{ $t('ui.common.edit') }} {{ $t('ui.common.device') }}: {{item.full_label}}
           <div class="pull-right">
             <action-details path="dashboard-devices" :id="item.id" size="regular"/>
             <template v-if="item.status == 1">
@@ -26,7 +26,7 @@
         </div>
         <div class="card-body">
           Device: {{id}}
-          {{item}}
+          {{item.full_label}}
         </div>
       </card>
     </div>
@@ -54,81 +54,8 @@ export default {
   },
   computed: {
     item () {
-      return Device.find(this.id)
+      return Device.query().where('id', this.id).first()
     },
-  },
-
-  methods: {
-    handleDelete(row) {
-      this.$swal({
-        title: this.$t('ui.prompt.delete_device'),
-        text: this.$t('ui.phrase.cannot_undo'),
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn-success btn-fill',
-        cancelButtonClass: 'btn btn-danger btn-fill',
-        confirmButtonText: 'Yes, delete it!',
-        buttonsStyling: false
-      }).then(result => {
-        if (result.value) {
-          this.$store.dispatch('yombo/devices/delete', row.id);
-          this.$swal({
-            title: this.$t('ui.common.deleted'),
-            text: `You deleted ${row.full_name}`,
-            type: 'success',
-            confirmButtonClass: 'btn btn-success btn-fill',
-            buttonsStyling: false
-          });
-        }
-      });
-    },
-    async handleEnable(row) {
-      await this.$swal({
-        title: this.$t('ui.prompt.enable_device'),
-        text: this.$t('ui.phrase.gateway_maybe_need_rebooted_after_change'),
-        type: 'info',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn-success btn-fill',
-        cancelButtonClass: 'btn btn-danger btn-fill',
-        confirmButtonText: 'Yes, enable it!',
-        buttonsStyling: false
-      }).then(result => {
-        if (result.value) {
-          let results = this.$store.dispatch('yombo/devices/enable', row.id);
-          this.$swal({
-            title: this.$t('ui.common.enabled'),
-            text: `You enabled ${row.full_name}`,
-            type: 'success',
-            confirmButtonClass: 'btn btn-success btn-fill',
-            buttonsStyling: false
-          });
-        }
-      });
-    },
-    async handleDisable(row) {
-      await this.$swal({
-        title: this.$t('ui.prompt.disable_device'),
-        text: this.$t('ui.phrase.gateway_maybe_need_rebooted_after_change'),
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn-success btn-fill',
-        cancelButtonClass: 'btn btn-danger btn-fill',
-        confirmButtonText: 'Yes, disable it!',
-        buttonsStyling: false
-      }).then(result => {
-        if (result.value) {
-          let results = this.$store.dispatch('yombo/devices/disable', row.id);
-          this.$swal({
-            title: this.$t('ui.common.disabled'),
-            text: `You disabled ${row.full_name}`,
-            type: 'success',
-            confirmButtonClass: 'btn btn-success btn-fill',
-            buttonsStyling: false
-          });
-        }
-      });
-    },
-
   },
   mounted () {
     this.$store.dispatch('yombo/devices/fetchOne', this.id);
